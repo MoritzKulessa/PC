5611791638adad70961e4cfeb378fd5811fbad16
moritz <moritz@LAPTOP-TOQCMULL>
2023-04-17 22:18:04 +0200

    Fixed type hints, imports and removed exceptions

--- src/probabilistic_circuits/pc_basics.py
+++ src/probabilistic_circuits/pc_basics.py
@@ -1,31 +1,32 @@
-import itertools
 import numpy as np
 import collections
-from functools import reduce
-from operator import add, mul
 from typing import Type, Callable
-from probabilistic_circuits.pc_nodes import PCNode, PCInnerNode, PCSum, PCProduct, PCLeaf, OffsetLeaf
+from probabilistic_circuits.pc_nodes import PCNode, PCInnerNode, PCSum, PCProduct
 
 
-def apply(pc: PCNode, func: Callable[[PCNode], None], node_type: Type[PCNode] = PCNode):
+def apply(pc: PCNode, func: Callable[[PCNode], None], node_type: Type[PCNode] = PCNode) -> None:
     """Applies a function on all nodes of a specific type in the given circuit."""
     queue = collections.deque([pc])
     while queue:
         node = queue.popleft()
         if isinstance(node, node_type):
             func(node)
+        if isinstance(node, PCInnerNode):
+            for child in node.children:
+                queue.append(child)
 
 
-def get_nodes_by_type(pc: PCNode, node_type: Type[PCNode] = PCNode):
+def get_nodes(pc: PCNode, node_type: Type[PCNode] = PCNode) -> list[PCNode]:
     """Returns all nodes of a specific type of circuit (including replicates)."""
     nodes = []
-    def get_nodes(node: PCNode):
+
+    def _get_nodes(node: PCNode):
         nodes.append(node)
-    apply(pc, get_nodes, node_type=node_type)
+    apply(pc, _get_nodes, node_type=node_type)
     return nodes
 
 
-def update_scope(pc: PCNode):
+def update_scope(pc: PCNode) -> set[object]:
     """Updates the scopes of all inner nodes in the given circuit (inplace)."""
     if isinstance(pc, PCInnerNode):
         pc.scope = set.union(*[update_scope(child) for child in pc.children])
@@ -33,7 +34,7 @@ def update_scope(pc: PCNode):
     return pc.scope
 
 
-def check_validity(pc: PCNode):
+def check_validity(pc: PCNode) -> None:
     """
     Checks the validity of the given circuit. Iterates over inner nodes and checks if their class variables have the
     correct type and that they do not have any children which are None. For product nodes decomposability is checked.
@@ -44,12 +45,11 @@ def check_validity(pc: PCNode):
             assert (len(node.children) > 0)
             assert (isinstance(node.children, list))
             for j, child in enumerate(node.children):
-                if child is None:
-                    raise Exception("{} has a child which is None (node number {} of {} in total)!".format(node, j, len(node.children)))
-                if not child.scope.issubset(node.scope):
-                    raise Exception("{} is not decomposable! Scope of child {} is not a subset of its parents scope!".format(node, child))
-                if len(node.scope) - len(child.scope) != len(node.scope - child.scope):
-                    raise Exception("{} is not decomposable! Children have overlapping scopes!".format(node))
+                assert (child is not None)
+                assert (isinstance(child, PCNode))
+                assert (child.scope.issubset(node.scope))
+                assert (len(node.scope) - len(child.scope) == len(node.scope - child.scope))
+            assert (node.scope == set.union(*[child.scope for child in node.children]))
 
         elif isinstance(node, PCSum):
             assert (np.isclose(sum(node.weights), 1))
@@ -58,8 +58,9 @@ def check_validity(pc: PCNode):
             assert (len(node.children) == len(node.weights))
             assert (len(node.children) > 0)
             for j, child in enumerate(node.children):
-                if child is None:
-                    raise Exception("{} has a child which is None (node number {} of {} in total)!".format(node, j, len(node.children)))
+                assert (child is not None)
+                assert (isinstance(child, PCNode))
+            assert (node.scope == set.union(*[child.scope for child in node.children]))
         else:
             raise Exception("Unknown node: " + str(node))
 
